6b5510c6b71a09395c2b9e81ba329615a7e7ba3a
rahulbandekar <rahulbandekar12@gmail.com>
2026-06-24 19:47:24 +0530

    chore(mock): start msw worker in development mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function prepareApp() {
+  if (import.meta.env.DEV) {
+    const { worker } = await import("./mocks/browser");
+    await worker.start({
+      onUnhandledRequest: "bypass",
+    });
+  }
+}
+
+prepareApp().then(() => {
+  const root = document.getElementById("root");
+  if (!root) throw new Error("Root element not found");
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
